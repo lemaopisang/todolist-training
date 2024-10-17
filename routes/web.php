c853e2d9ff94a57827ba13a002379960ae08c16f
lemaopisang <arkandi.2020@gmail.com>
2024-10-17 09:42:10 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('todos',
+\App\Http\Controllers\TodoController::class);
+
+Route::get('todos/{todo}',
+[\App\Http\Controllers\TodoController::class,'destroy'])->name('todos.destroy');
